bab312434fd2cd478f0a48a8865f08d058e35df4
Krav_no0r@student.itstep.org <Krav_no0r@student.itstep.org>
2022-05-17 14:03:07 +0300

    add code

--- zd2.h
+++ zd2.h
@@ -1,1 +1,25 @@
-#pragma once
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int arrlen = 10;
+    int arr[arrlen];
+    int* ret = arr;
+
+    for (int i = 0; i < arrlen; i++)
+    {
+        cout << "arr[" << i << "] = ";
+        cin >> ret[i];
+    }
+
+    cout << "[ ";
+    for (int i = 0; i < arrlen; i++)
+        cout << ret[i] << " ";
+    cout << "]" << endl;
+
+    cout << "[ ";
+    for (int i = arrlen - 1; i >= 0; i--)
+        cout << ret[i] << " ";
+    cout << "]" << endl;
+    return 0;
+}
