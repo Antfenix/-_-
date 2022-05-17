f6dc72ec746d824aba1f94e15b191bc8eabaa70e
Krav_no0r@student.itstep.org <Krav_no0r@student.itstep.org>
2022-05-17 13:42:35 +0300

    add code

---
+++ zd1.h
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    const int size = 10;
+    int arr1[size] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+    int arr2[size];
+    int* ptr_arr1 = arr1;
+    int* ptr_arr2 = arr2;
+
+    cout << "{ ";
+    for (int i = 0; i < size; i++)
+    {
+        ptr_arr2 = ptr_arr1;
+        cout << *ptr_arr2 << " " << endl;
+        ptr_arr1++;
+        ptr_arr2++;
+    }
+    cout << "}\n\n";
+
+    system("PAUSE");
+}
